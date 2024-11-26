8faf0a666ac1bc2f41c0324374e70747fdfc68cf
George G <ggpsgeorge@gmail.com>
2024-11-26 16:08:17 -0300

    scroller trial

--- src/main/client/fullstack_hospital_system_fhir_frontend/src/App.jsx
+++ src/main/client/fullstack_hospital_system_fhir_frontend/src/App.jsx
@@ -1,26 +1,18 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import Fetch from './Fetch';
+import Profile from './Profile';
+import Scroller from './Scroller';
 
 function App() {
 
-  const [patient, setPatient] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8181/api/patient/v1/patient-1')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setPatient(data);
-      });
-  }, []);
-
-  return (
-    <div>
-      {JSON.stringify(patient)}
-    </div>
-  )
+  return(
+    <>
+      <Fetch></Fetch>
+      <Profile></Profile>
+      <Scroller></Scroller>
+    </>
+  );
+  
 }
 
 export default App
