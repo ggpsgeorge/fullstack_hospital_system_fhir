83228948ac79912a16d5c27900ad64b15f23e644
George G <ggpsgeorge@gmail.com>
2024-12-09 14:49:46 -0300

    use of a select tag to get a resourceType option to the Scroller component

--- src/main/client/fullstack_hospital_system_fhir_frontend/src/App.jsx
+++ src/main/client/fullstack_hospital_system_fhir_frontend/src/App.jsx
@@ -5,12 +5,25 @@ import Scroller from './Scroller';
 import { useState, useEffect } from 'react';
 
 function App() {
-    
+  
+  const [resourceType, setResourceType] = useState("Patient")
+  const handleResourceTypeChange = (event) => {
+    setResourceType(event.target.value)
+  }
+
   return(
     <>
-      <Profile name="Profile Name"></Profile>
-      <Scroller resourceType="Patient"></Scroller>
-      <Scroller resourceType="Practioner"></Scroller>
+      {/* <Profile name="Profile Name"></Profile> */}
+      <h2>Hospital Management System</h2>
+      <select name={resourceType} onChange={handleResourceTypeChange}>
+        <option>Select Data Type</option>
+        <option value="Patient">Patients</option>
+        <option value="Practioner">Doctors</option>
+        <option value="Organization">Hospitals</option>
+      </select>
+      <p>Resource Type: {resourceType} </p>
+      <Scroller resourceType={resourceType}></Scroller>
+      {/* <Scroller resourceType="Practioner"></Scroller> */}
     </>
   );
   
